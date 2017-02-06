ad75f8039fe9068d7709cf77b6475e0383df0d33
John Hopkins <johnthopkins@gmail.com>
2017-02-06 09:43:07 -0800

    small cleanup

--- spec_2/index.js
+++ spec_2/index.js
@@ -1,35 +1,12 @@
-const letters = ['_','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
-'P','Q','R','S','T','U','V','W','X','Y','Z']
+  const letters = ['_','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
+  'P','Q','R','S','T','U','V','W','X','Y','Z']
 
-const letterValues = [
-  ['_', 2, 0],
-  ['A', 9, 0],
-  ['B', 2, 0],
-  ['C', 2, 0 ],
-  ['D', 4, 0],
-  ['E', 12, 0],
-  ['F',2, 0],
-  ['G',3, 0],
-  ['H',2, 0],
-  ['I',9, 0],
-  ['J',1, 0],
-  ['K',1, 0],
-  ['L',4, 0],
-  ['M',2, 0],
-  ['N',6, 0],
-  ['O',8, 0],
-  ['P',2, 0],
-  ['Q', 1, 0],
-  ['R', 6, 0],
-  ['S',4, 0],
-  ['T',6, 0],
-  ['U',4, 0],
-  ['V', 2, 0],
-  ['W',2, 0],
-  ['X',1, 0],
-  ['Y',2, 0],
-  ['Z',1, 0]
-]
+  const letterValues = [
+    ['_', 2 ],['A', 9 ],['B', 2 ],['C', 2 ],['D', 4 ],['E', 12],
+    ['F', 2],['G', 3],['H', 2],['I', 9],['J', 1],['K', 1],['L', 4],['M', 2],['N', 6],
+    ['O', 8],['P', 2],['Q', 1],['R', 6],['S', 4],['T', 6],['U', 4],['V', 2],['W', 2],
+    ['X', 1],['Y', 2],['Z', 1]
+  ]
 
 
   const frequencies = [[],[],[],[],[],[],[],[],[],[],[],[],[]]
@@ -43,10 +20,9 @@ const letterValues = [
   }
 
   const calculateNewFrequency = character => {
-    let indexOfCharacter = letters.findIndex(a => a === character)
-    let values = letterValues[indexOfCharacter]
-    let previousFrequencyIndex = values[1]
+    let previousFrequencyIndex = letterValues[letters.findIndex(a => a === character)][1]
     let newFrequencyIndex
+
     if(previousFrequencyIndex > 0){
       newFrequencyIndex = previousFrequencyIndex - 1
     } else { console.log('Someone hid a tile up their sleeve!! Too many', `'`, values[0],`'`, 'tiles' ); return}
@@ -56,7 +32,7 @@ const letterValues = [
     let targetFrequency = frequencies[newFrequencyIndex]
     targetFrequency.push(removedItem[0])
     values[1] -= 1
-    values[2] += 1
+    values[2] += 1  
   }
 
   const adjustFrequencies = (string) => {
@@ -64,12 +40,12 @@ const letterValues = [
     arrayOfCharacters.forEach(calculateNewFrequency)
   }
 
-const remainingTiles = (usedString) => {
-  setFrequencies()
-  adjustFrequencies(usedString)
-  for(i = frequencies.length - 1 ; i > 0; i--){
-    console.log('i', i, 'freq', frequencies[i])
+  const remainingTiles = (usedString) => {
+    setFrequencies()
+    adjustFrequencies(usedString)
+    for(i = frequencies.length - 1 ; i > 0; i--){
+      console.log('i', i, 'freq', frequencies[i])
+    }
   }
-}
 
 module.exports = remainingTiles
